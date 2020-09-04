752c77b56a651492eb1c5e1238825c1c8cb77c7d
Juan Oxoby <me@jmoxo.by>
2020-09-03 18:59:21 -0700

    Fixes tinyxml and uordfdom

--- recipes/urdfdom/all/conanfile.py
+++ recipes/urdfdom/all/conanfile.py
@@ -12,6 +12,7 @@ class URDFDom(ConanFile):
     topics = ("robotics", "simulation")
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package"
+    exports_sources = ['patches/*']
 
     @property
     def _source_subfolder(self):
@@ -24,6 +25,7 @@ class URDFDom(ConanFile):
     def requirements(self):
         for req in self.conan_data["requirements"]:
             self.requires(req)
+        self.options["tinyxml"].with_stl = True
 
     def _configure_cmake(self):
         cmake = CMake(self)
@@ -32,6 +34,9 @@ class URDFDom(ConanFile):
         return cmake
 
     def build(self):
+        for patch in self.conan_data["patches"].get(self.version, []):
+            tools.patch(**patch)
+
         cmake = self._configure_cmake()
         cmake.build()
 
